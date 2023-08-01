3e45e2e09c157a83cbe0c2ddfdf15f8bdfd23632
abbas-abdalhusain-yosif <abs_abdal@yahoo.com>
2023-08-01 07:05:17 -0700

    solve task : error handling/ date : 1-8-2023

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-function divideNumbers(a, b) {}
+function divideNumbers(a, b) {
+  try
+  {
+    if(typeof a !== "number" || typeof b !== "number")
+    {
+      throw new Error("either a or b is not Number");
+      
+    }
+    if(b === 0)
+    {
+      throw new Error("not allowed to divide by 0");
+     
+    }
+    console.log(a/b);
+  }catch(error)
+  {
+    console.error(error.message);
+  }
+  
+}
 
 // Test cases
-try {
+
   console.log(divideNumbers(10, 2)); // should print 5
   console.log(divideNumbers(15, 0)); // should throw an error
   console.log(divideNumbers("hello", 7)); // should throw an error
   console.log(divideNumbers(12)); // should throw an error
-} catch (error) {
-  console.error(error.message);
-}
