432ff34996ed267ac29d3988aabcfcdc686eaaa4
haneenjaleel <haneenjaleel7@gmail.com>
2023-08-01 17:17:46 +0300

    erroe handling

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-function divideNumbers(a, b) {}
-
-// Test cases
-try {
+function divideNumbers(a, b) {
+try {  
+   if(typeof a!=="number" || typeof b!=="number"){
+throw new Error("Both arguments must be numbers");
+}
+if(b===0){
+throw new Error("Cannot divide by zero");
+}
+return a/b;
+} 
+ catch (error) {
+  console.error(error.message);
+}}
   console.log(divideNumbers(10, 2)); // should print 5
   console.log(divideNumbers(15, 0)); // should throw an error
   console.log(divideNumbers("hello", 7)); // should throw an error
   console.log(divideNumbers(12)); // should throw an error
-} catch (error) {
-  console.error(error.message);
-}
+
