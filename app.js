749395b47a8038e85e5267092bdf8ee37cc38ed4
walidemad <walidemad313@gmail.com>
2023-08-04 18:20:35 +0300

    error handling

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-function divideNumbers(a, b) {}
+function divideNumbers(a, b) {
+  try{
+
+  if (typeof a !== "number" || typeof b !== "number")
+  throw "value not a number"
+  if(b === 0) throw 'Cannot divide by zero'
+  if (!b) throw 'add the another number'
+    console.log(a/b);
+  }
+  catch (error) {
+    console.error(error);
+  }
+
+}
 
 // Test cases
 try {
+  
   console.log(divideNumbers(10, 2)); // should print 5
   console.log(divideNumbers(15, 0)); // should throw an error
   console.log(divideNumbers("hello", 7)); // should throw an error
