1fe3e9cd63a349bc05218ef4176e09e5102135a8
zainabthabit <zainabthabit886@gmail.com>
2024-02-27 17:32:19 +0300

    Error handling task solved

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 function divideNumbers(a, b) {
-// Test cases
-
-
+if (typeof a !== "number" || typeof b!=="number"){
+  throw new Error('a and b must be numbers');
+}else if(b===0){
+  throw new Error('Cannot divide number by zero');
+}
+return a/b
 }
 
-try {
+  try {
   console.log(divideNumbers(10, 2)); // should print 5
+} catch (error) {
+  console.error('error' ,error.message);
+}
+try {
   console.log(divideNumbers(15, 0)); // should throw an error
+} catch (error) {
+  console.error('error' ,error.message);
+}
+try {
+
   console.log(divideNumbers("hello", 7)); // should throw an error
+} catch (error) {
+  console.error('error' ,error.message);
+}
+try {
   console.log(divideNumbers(12)); // should throw an error
 } catch (error) {
-  console.error(error.message);
+  console.error('error' ,error.message);
 }
