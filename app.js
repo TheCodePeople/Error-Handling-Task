e3562187f031a54190d2d342888f9d44f60fcfcb
Mustafa88A <mustafa0780500@gmail.com>
2024-02-27 18:08:32 +0300

    task Error 2024

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-function divideNumbers(a, b) {
-  if (typeof a !== "number" || b !== "number") {
+function divideNumbers(num1, num2) {
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
     throw new Error("Both arguments must be numbers");
   }
-  if (b === 0) {
-    throw new Error("Cannot divide by zero ");
+  if (num2 === 0) {
+    throw new Error("Cannot divide by zero");
   }
-  return a / b;
+  return num1 / num2;
 }
 
-// Test cases
 try {
   console.log(divideNumbers(10, 2)); // should print 5
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
   console.log(divideNumbers(15, 0)); // should throw an error
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
   console.log(divideNumbers("hello", 7)); // should throw an error
+} catch (error) {
+  console.error(error.message);
+}
+try {
   console.log(divideNumbers(12)); // should throw an error
 } catch (error) {
   console.error(error.message);
